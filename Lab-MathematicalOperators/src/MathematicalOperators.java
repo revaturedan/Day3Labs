bc60c2e2807bab8ad693026901fd61008c94c99b
revaturedan <86729721+revaturedan@users.noreply.github.com>
2021-07-05 16:26:24 -0500

    Add files via upload

---
+++ Lab-MathematicalOperators/src/MathematicalOperators.java
@@ -0,0 +1,36 @@
+public class MathematicalOperators {
+    public static void main(String[] args){
+        //define variables
+        int x = 8;
+        int y = 5;
+        int z;
+
+        //simple addition
+        z = x + y;
+        System.out.println(z);
+
+        //simple subtraction
+        z = x - y;
+        System.out.println(z);
+
+        //simple multiplication
+        z = x * y;
+        System.out.println(z);
+
+        //simple division
+        z = x / y;
+        System.out.println(z);
+
+        //simple modulus
+        z = x % y;
+        System.out.println(z);
+
+        //float vs. int
+        float f = 4.6f;
+        int i = 34;
+        double result = f + i;
+
+        System.out.println(result);
+        }
+    }
+
